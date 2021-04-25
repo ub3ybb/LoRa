1113feaeb1faf9f0a6d526785df089ac1e748dfa
German <56148732+ub3ybb@users.noreply.github.com>
2021-04-25 19:59:27 +0300

    +

---
+++ FIRMWARE/LORA_V3_NEW_DESIGN/Debug/Core/Startup/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Core/Startup/startup_stm32l152rbtx.s 
+
+OBJS += \
+./Core/Startup/startup_stm32l152rbtx.o 
+
+S_DEPS += \
+./Core/Startup/startup_stm32l152rbtx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Startup/startup_stm32l152rbtx.o: ../Core/Startup/startup_stm32l152rbtx.s
+	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -c -x assembler-with-cpp -MMD -MP -MF"Core/Startup/startup_stm32l152rbtx.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+
